cac5ce2b8e947febfe0132200bd7bb6942d91742
Ignacio R Espino <irespino@capozios.com>
2018-08-21 18:22:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,11 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  sleep 5
+def countdown_with_sleep(number)
+    while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
